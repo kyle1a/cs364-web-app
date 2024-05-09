c3b2d75a2d2d9347f482e00ac109455f6ae606e1
Bpriley14 <113140161+Bpriley14@users.noreply.github.com>
2024-05-08 18:28:02 -0600

    test.php

--- test.php
+++ test.php
@@ -1,59 +1,73 @@
 <!DOCTYPE html>
-
 <html>
-    <head>
+<head>
     <center>
-	<img src="logo.png" width="300" height="250"/>
-</center>
-        <title>Study Buddy Test</title>
-        <link rel="stylesheet" href="csl.css" id="styleSheet">
+        <img src="logo.png" width="300" height="250"/>
+    </center>
+    <title>Study Buddy Test</title>
+    <link rel="stylesheet" href="csl.css" id="styleSheet">
+</head>
+<body>
+    <center>
+        <nav>
+            <a href="index.html">Home Page</a> |
+            <a href="flashcards.html">Flashcards</a> |
+            <a href="questions.html">Create Questions</a> |
+            <a href="testpage.html">Generate Test</a> |
+            <a href="missedQuestions.html">Missed Questions</a> 
+        </nav>
+        <form action="missedQuestions.php", method="POST">
+        <?php
+            $servername = "localhost";
+            $username = "student";
+            $password = "CompSci364";
+            $database = "student";
+           
+           
+            $connect = new mysqli($servername, $username, $password, $database)
+            or die("Could not connect to database;" . mysqli_error());
+            $unit = $_POST["unit"];
+            echo "<h2> Unit $unit Test</h2>";                
+         
+            $SQL = "SELECT * FROM Questions WHERE unit = $unit;";
+            $result = $connect->query($SQL);
+         
             
-    </head>
-    <body>
-        <center>
-	<nav>
-        	<a href="index.html">Home Page</a> |
-        	<a href="flashcards.html">Flashcards</a> |
-        	<a href="questions.html">Create Questions</a> |
-        	<a href="testpage.html">Generate Test</a> |
-        	<a href="missedQuestions.html">Missed Questions</a> 
-	</nav>
-            <form action="missedQuestions.php", method="POST">
-            <?php
-                $servername = "localhost";
-                $username = "student";
-                $password = "CompSci364";
-                $database = "student";
-                
-                // Create connection
-                $connect = new mysqli($servername, $username, $password, $database)
-                    or die("Could not connect to database;" . mysqli_error());
-                
-                $unit = $_POST["unit"];
-                echo "<h2> Unit $unit Test</h2>";                
-
-                $SQL = "SELECT * FROM Questions WHERE unit = $unit;";
-                $result = $connect->query($SQL);
-
-                if ($result->num_rows > 0) {             
-                    // Output data for each row
-                    $a = 0;
-                    while ($row = $result->fetch_assoc()) { 
-                        echo "<p>" . htmlspecialchars($row["question"]) . "</p>";
-                        for ($i = 0; $i < 4; $i++) {
-                            if($i == 0){
-                                #make sure the answer is answer as $i == 0
-                            }
-                            echo "<input type=\"radio\" id=\"q$a$i\" name=\"q$a\" value=\"q$a$i\"> <label for=\"q$a$i\">" . htmlspecialchars($row["answer"]) . "</label>$a$i ";   
+           
+            $SQL_all_answers = "SELECT answer FROM Questions;";
+            $result_all_answers = $connect->query($SQL_all_answers);
+            $all_answers = [];
+            while ($row = $result_all_answers->fetch_assoc()) {
+                $all_answers[] = $row["answer"];
+            }
+         
+            if ($result->num_rows > 0) {             
+                // Output data for each row
+                $a = 0;
+                while ($row = $result->fetch_assoc()) { 
+                    echo "<p>" . htmlspecialchars($row["question"]) . "</p>";
+                    // Generate a set of answers for the question
+                    $answers = [$row["answer"]];
+                    while (count($answers) < 4) {
+                        $random_answer = $all_answers[array_rand($all_answers)];
+                        if (!in_array($random_answer, $answers)) {
+                            $answers[] = $random_answer;
                         }
-                        $a++;
-                    }         
-                } 
-
-            ?>
-            <br><br><input type="submit" value="Submit test"><br><br><br>
-            </form>
-        </center>
-
-    </body>
+                    }
+            
+                    shuffle($answers);
+                    
+                    echo '<div class="inline-radio">';
+                    for ($i = 0; $i < 4; $i++) {
+                        echo "<input type=\"radio\" id=\"q$a$i\" name=\"q$a\" value=\"q$a$i\"> <label for=\"q$a$i\">" . htmlspecialchars($answers[$i]) . "</label><div>";  
+                    }
+                    echo '</div>';
+                    $a++;
+                }         
+            }
+        ?>
+        <br><br><input type="submit" value="Submit test"><br><br><br>
+        </form>
+    </center>
+</body>
 </html>
