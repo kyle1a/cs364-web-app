3b85dfa89b0b79a11eb2ab80c05e17034fc2daa2
austinh567 <92406394+austinh567@users.noreply.github.com>
2024-05-08 20:31:33 -0600

    Update users.php

--- users.php
+++ users.php
@@ -1,4 +1,4 @@
-?php
+<?php
 	/* Embedding account credentials isn't ideal...preferable to
 	 * store in a separate file that is included by PHP (and not
 	 * accessible to others)
@@ -13,20 +13,27 @@
 		die("Connection failed: " . $conn->connect_error);
 	}
 	
+	
 	$username = $conn->real_escape_string($_POST['username']);
-	$password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+	$password = password_hash($_POST['password'], PASSWORD_DEFAULT); // Hash the password
 	$firstName = $conn->real_escape_string($_POST['firstName']);
 	$lastName = $conn->real_escape_string($_POST['lastName']);
-	$type = $conn->real_escape_string($_POST['type']);
+
+	$stmt = $conn->prepare("INSERT INTO users (username, pass, firstName, lastName) VALUES (?, ?, ?, ?)");
+	$stmt->bind_param("ssss", $username, $password, $firstName, $lastName);
 	
 
-	$data = array(
-      "INSERT INTO users.sql (firstName, lastName, username, password) VALUES ".
-          "('Ben', 'Riley', 'bpriley', 'password'), ",
-    );
+	$stmt = $conn->prepare("INSERT INTO users (username, pass, firstName, lastName) VALUES (?, ?, ?, ?)");
+	$stmt->bind_param("ssss", $username, $password, $firstName, $lastName);
     
-    
+	if ($stmt->execute()) {
+		echo "Question successfully updated!";
+		header("Location: login.php");
+	} else {
+		echo "Error: " . $stmt->error;
+	}
 	
+	$stmt->close();
 	$conn->close();
 ?>
 <!DOCTYPE html>
